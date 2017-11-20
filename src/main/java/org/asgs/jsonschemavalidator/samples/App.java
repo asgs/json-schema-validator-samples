25d4a84679581381c367634d959718038e225ed7
asgs <asgosa.eeegct@gmail.com>
2017-11-21 00:51:42 +0530

    added a simple schema to validate another simple json.

--- src/main/java/org/asgs/jsonschemavalidator/samples/App.java
+++ src/main/java/org/asgs/jsonschemavalidator/samples/App.java
@@ -1,12 +1,41 @@
 package org.asgs.jsonschemavalidator.samples;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.LogLevel;
+import com.github.fge.jsonschema.core.report.ProcessingMessage;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
 /**
- * Hello world!
+ * Sample demo to use the json-schema-validator library to validate a JSON doc
+ * against a predefined JSON Schema.
  *
+ * @author asgs
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException, ProcessingException {
+        JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+        String json = "{\"x\":1}";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(json);
+        InputStream schemaInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("schema.json");
+        JsonSchema jsonSchema = factory.getJsonSchema(mapper.readTree(schemaInputStream));
+        ProcessingReport processingReport = jsonSchema.validate(jsonNode);
+        System.out.println("----------------");
+        processingReport.forEach(m -> {
+                    if (m.getLogLevel() == LogLevel.ERROR) {
+                        JsonNode errorNode = m.asJson();
+                        System.out.println("The field " + errorNode.get("instance").get("pointer").asText() + " of " + errorNode.get("message").asText());
+                    }
+                }
+        );
     }
 }
 
